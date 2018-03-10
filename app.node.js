38adbbddd1e0df54df795df511e09a46e1d6e1fa
Michela Lorandi <michela.lorandi@studenti.unitn.it>
2018-03-10 16:49:46 +0100

    aggiunta file

---
+++ app.node.js
@@ -0,0 +1,18 @@
+//Istanza Express
+var express = require('express');
+var app = express();
+
+//Istanza bodyparser per leggere i JSON
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+var listaReq = [];
+
+app.post('/assistente', function(req, res, next) {
+  listaReq.push(req);
+  res.sendStatus(200);
+});
+
+app.get('/', function(req, res, next) {
+  res.send(listaReq);
+});
