9b9b5696d8af6f44dba17af44cc4f660595cadfb
Michela Lorandi <michela.lorandi@studenti.unitn.it>
2018-03-10 17:23:50 +0100

    porta sbagliata

--- server.node.js
+++ server.node.js
@@ -2,8 +2,8 @@ const app = require('./app.node.js');
 
 //apro server su porta 8080
 //heroku vuole ascoltare sulla sua porta
-//var porta = process.env.PORT || 3000;
-var porta = 8080;
+var porta = process.env.PORT || 3000;
+//var porta = 8080;
 app.listen(porta, function () {
     console.log('Server aperto');
 });
